cc9aed889b39137d2d6ee9beb7265a641f559684
Anuskapanda-231 <pandaanuska231@gmail.com>
2026-05-19 16:10:07 +0530

    Time: 2 ms (97.84%) | Memory: 45.1 MB (13.38%) - LeetSync

--- Leetcode/Arrays/1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ Leetcode/Arrays/1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -1,31 +1,24 @@
-import java.util.Arrays;
+import java.util.HashSet;
 
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        Arrays.sort(arr);
+        HashSet<Integer> set = new HashSet<>();
 
-        for (int i = 0; i < arr.length; i++) {
-            int target = 2 * arr[i];
+        for (int num : arr) {
 
-            int idx = binarySearch(arr, target);
-
-            if (idx != -1 && idx != i) {
+           
+            if (set.contains(num * 2)) {
                 return true;
             }
+
+           
+            if (num % 2 == 0 && set.contains(num / 2)) {
+                return true;
+            }
+
+            set.add(num);
         }
+
         return false;
-    }
-
-    private int binarySearch(int[] arr, int target) {
-        int left = 0, right = arr.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (arr[mid] == target) return mid;
-            else if (arr[mid] < target) left = mid + 1;
-            else right = mid - 1;
-        }
-        return -1;
     }
 }
