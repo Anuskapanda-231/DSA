749c3c57bf9dbfeff72689025324ea9636b5cd85
Anuskapanda-231 <pandaanuska231@gmail.com>
2026-05-19 18:25:25 +0530

    Time: 4 ms (78.82%) | Memory: 43.5 MB (42.43%) - LeetSync

---
+++ Leetcode/Arrays/1137-height-checker/height-checker.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        
+        int n = heights.length , count =0;
+        
+        int [] expected = heights.clone();
+        Arrays.sort(expected);
+        
+        for(int i=0;i<n;i++){
+           if( heights[i]!=expected[i])
+               count++;
+        }
+        
+        return count;
+        
+    }
+}
